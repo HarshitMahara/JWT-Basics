d8d2f1226fc5bcb7230af5a86dd2cac166a76788
Harshit Singh Mahara <maharaharshit31@gmail.com>
2024-07-16 12:16:29 +0530

    change

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import "dotenv/config";
 import dbConnect from "./dbConnect.js";
+import "express-async-errors";
+import notFound from "./middleware/notFound.js";
+import { errorHandler } from "./middleware/errorHandler.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +12,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("<h1>JWT BASICS</h1>");
 });
+
+app.use(notFound)
+app.use(errorHandler)
 
 (async () => {
   try {
